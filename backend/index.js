5c3946c4fb06f781fa8426f3be1090e1c9ddc107
Kaalaayaa <kalaya.chatelain@yahoo.fr>
2021-10-28 11:04:58 +0200

    api added + frontend test

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import connect from "./libs/config.js";
 import config from './libs/config.js';
+import axios from "axios"
 
 
 // Setup / Configure Express
@@ -8,10 +9,25 @@ const app = express();
 config(app)
 connect(app)
 
+var options = {
+  method: 'GET',
+  url: 'https://50k-radio-stations.p.rapidapi.com/get/channels',
+  params: {keyword: 'a', country_id: '50', page: '1'},
+  headers: {
+    'x-rapidapi-host': '50k-radio-stations.p.rapidapi.com',
+    'x-rapidapi-key': '1d26f359d5mshd8151fbb9814149p1e79f5jsned2a629458dd'
+  }
+};
+
 app.get("/api", (req, res) => {
-    res.json({ success: true})
+axios.request(options).then(function (response) {
+	res.json({ station : response.data.data[0].streams_url[0].url});
+}).catch(function (error) {
+	console.error(error);
+});
+
 })
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 9124
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
