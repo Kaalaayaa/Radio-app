fff4886b6eb6ff7766c97b4e4587a8cb0b7368d0
Mxx1029 <lia.pim@icloud.com>
2021-10-29 14:13:33 +0200

    2nd commit : Add frontend components, move h1+radio to Radio component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,53 +1,19 @@
 import "./App.css";
-import { useState } from "react";
-import { useEffect } from "react";
+import Login from "./components/Login.jsx";
+import Register from "./components/Register.jsx";
+import Radio from "./components/Radio.jsx";
+import DeleteAccount from "./components/DeleteAccount.jsx";
+import Comments from "./components/Comments.jsx";
 
 export default function App() {
-  const [station, setStation] = useState("");
-  const [city, setCity] = useState("");
-  const url = `http://localhost:9125/city/${city}`;
   
-   
-  useEffect(() => {
-      city.length > 1 ? 
-        fetch(url)
-        .then((response) => response.json())
-        .then((station) => {
-          setStation(station.station);
-        })
-        .catch((error) => {
-          alert(
-            "Oh no! An error happened! Please try again. If the problem persists, contact support at 555-123-456 :)"
-          );
-          console.error(error);
-        }) : null
-      }, [city]); 
-
-//   function radioChange(city) {
-    
-//   }
-
   return (
     <div className="App">
-      <h1>Select a city to listen to a local radio station</h1>
-      <div className="select">
-        <select
-          onChange={(e) => {
-            setCity(e.target.value);
-            // radioChange(e.target.value);
-          }}
-          value={city}
-          name="format"
-          id="format"
-        >
-          <option value="">Choose a city</option>
-          <option value="wellington">Wellington</option>
-          <option value="fortDeFrance">Fort-de France</option>
-          <option value="madrid">Madrid</option>
-        </select>
-      </div>
-      {/* <button onClick={handleClick}>Station</button> */}
-      <audio src={station} controls autoPlay />
+      <Login />
+      <Register />
+      <DeleteAccount />
+      <Radio />
+      <Comments />
     </div>
   );
 }
